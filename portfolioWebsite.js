416dfa2ef0f96b5162a63a1bfbb89f0b3326e690
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-06 12:50:03 -0400

    Nevin testing

--- portfolioWebsite.js
+++ portfolioWebsite.js
@@ -1,12 +1,12 @@
 let i = 0;
-let text = "Web Developer";
+let text = "Charles Nesmith II";
 let typeSpeed = 50;
 
 function writeToScreen() {
     if(i < text.length) {
         document.getElementById("developer").innerHTML += text.charAt(i);
         i++;
-        setTimeout(writeToScreen, typespeed);
+        setTimeout(writeToScreen, typeSpeed);
     }
 }
 writeToScreen();
