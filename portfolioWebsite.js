306f51d787999557033f46c3ab2a9f67bca13d5c
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-07 13:23:46 -0400

    created 3 div elements inside of the home section with the class names of: center-text, blue, and yellow. Added CSS animations to the yellow and blue classes. Set background color of the home section to black. Anmiations for the yellow and blue classes move the elements from the center of the screen to the edge of the screen and back infinitely. All div elements are set to 50% border radius as to reflect circles moving from side to side

--- portfolioWebsite.js
+++ portfolioWebsite.js
@@ -1,12 +1,15 @@
-let i = 0;
-let text = "Charles Nesmith II";
-let typeSpeed = 50;
+// let i = 0;
+// let creativeDev = "Creative Full-Stack Developer";
+// let typeSpeed = 50;
 
-function writeToScreen() {
-    if(i < text.length) {
-        document.getElementById("developer").innerHTML += text.charAt(i);
-        i++;
-        setTimeout(writeToScreen, typeSpeed);
-    }
-}
-writeToScreen();
+// function writeToScreen() {
+//     if(i < creativeDev.length) {
+//         document.getElementById("devDesc").innerHTML += creativeDev.charAt(i);
+//         i++;
+//         setTimeout(writeToScreen, typeSpeed);
+//     }
+// }
+// writeToScreen();
+
+
+
